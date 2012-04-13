5d1ab91f7647b99e16d4b4a9aae3c3e3ea56c641
Ryan Howe <ryan@mealtracker.com>
2012-04-13 14:10:57 -0400

    -- updated Metal and seeds.db to fetch metal names

--- app/models/metal.rb
+++ app/models/metal.rb
@@ -1,4 +1,29 @@
 class Metal < ActiveRecord::Base
+  require 'open-uri'
+  require 'nokogiri'  
   attr_accessible :name
   validates_presence_of :name
+
+def price
+	current_price = 0
+	@doc = Nokogiri::HTML(open("http://finance.yahoo.com/futures?t=metals"))
+	@doc.css("#yfitp table tr").each do |tr|
+		p = tr.css('.last_trade span').first.content
+		metal = tr.css('td.second.name').text		
+		if metal.include?'.'
+			metal_name, metal_weight, metal_unit, month, day = metal.split(' ')
+			metal_name = "#{metal_name} #{metal_weight} #{metal_unit}"
+			date = "#{month} #{day}"
+		else
+			metal_name, month, day = metal.split(' ')
+			date = "#{month} #{day}"
+		end
+		if self.name==metal_name
+			current_price=p
+		end
+	end
+	current_price
+end
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,25 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-metals = Metal.create([{name: 'Copper'}, {name: 'Gold 100oz'}, {name: 'Gold'}, {name: 'Palladium'}, {name: 'Platinum'}, {name: 'Silver 5000oz'}, {name: 'Silver'}])
+require 'date'
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+@doc = Nokogiri::HTML(open("http://finance.yahoo.com/futures?t=metals"))
+@doc.css("#yfitp table tr").each do |tr|
+	price = tr.css('.last_trade span').first.content
+	metal = tr.css('td.second.name').text		
+	if metal.include?'.'
+		metal_name, metal_weight, metal_unit, month, day = metal.split(' ')
+		metal_name = "#{metal_name} #{metal_weight} #{metal_unit}"
+		date = "#{month} #{day}"
+	else
+		metal_name, month, day = metal.split(' ')
+		date = "#{month} #{day}"
+	end
+	Metal.create(:name => metal_name)
+end 
+
+
+#metals = Metal.create([{name: 'Copper'}, {name: 'Gold 100oz'}, {name: 'Gold'}, {name: 'Palladium'}, {name: 'Platinum'}, {name: 'Silver 5000oz'}, {name: 'Silver'}])
