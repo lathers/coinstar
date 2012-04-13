d41da7f32edd548a4b408562439def373e548d73
Ryan Howe <ryan@appthecloud.com>
2012-04-12 20:37:11 -0400

    --initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coinstar::Application.config.secret_token = '8e75511a36b8842dc0f225af209ffb7e02e265812e1c8bd64d89006dc7e6ac08d488ef864023b10de8a8d96d97e40dca158f7a7152105f93922b99eebe9cf428'
